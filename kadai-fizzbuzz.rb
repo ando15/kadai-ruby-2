45b4d0cb77fdc190aacd1c706d82ed080b8b581b
tomoya.ando <tomoya.ando@techacademy.jp>
2022-08-03 07:13:11 +0000

    ruby-kadai-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
-def FizzBuzz
-  num_max = 100
-  (1..num_max).each do |number|
-   if number%3 == 0 && number%5 == 0
-     puts "FizzBuzz"
-   elsif number%5 == 0
-     puts "Buzz"
-   elsif number%3 == 0
-     puts "Fizz"
-   else
-     puts number
-   end
- end
+def fizzbuzz(num)
+    if num%3 == 0 && num%5 == 0
+        "FizzBuzz"
+    elsif num%5 == 0
+        "Buzz"
+    elsif num%3 == 0
+        "Fizz"
+    else
+        num
+    end
 end
 
-FizzBuzz()
+num_max = 100
+(1..num_max).each do |number|
+    puts fizzbuzz(number)
+end
